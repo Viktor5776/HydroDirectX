bf44cde44e7956968ce332e194d81e3a1f71627b
viktor schewenius <viktorschewenius@gmail.com>
2023-01-01 19:59:20 +0100

    Started to use ComPtr insted of normal pointer

--- HydroDirectX/Graphics.cpp
+++ HydroDirectX/Graphics.cpp
@@ -62,30 +62,9 @@ Graphics::Graphics( HWND hWnd )
 	) );
 
 	//Access Backbuffer in SwapChain
-	ID3D11Resource* pBackBuffer = nullptr;
-	GFX_THROW_INFO( pSwap->GetBuffer( 0,__uuidof(ID3D11Texture2D),reinterpret_cast<void**>(&pBackBuffer) ) );
-	GFX_THROW_INFO( pDevice->CreateRenderTargetView( pBackBuffer,nullptr,&pTarget ) );
-	pBackBuffer->Release();
-}
-
-Graphics::~Graphics()
-{
-	if( pTarget != nullptr )
-	{
-		pTarget->Release();
-	}
-	if( pContext != nullptr )
-	{
-		pContext->Release();
-	}
-	if( pSwap != nullptr )
-	{
-		pSwap->Release();
-	}
-	if( pDevice != nullptr )
-	{
-		pDevice->Release();
-	}
+	Microsoft::WRL::ComPtr<ID3D11Resource> pBackBuffer;
+	GFX_THROW_INFO( pSwap->GetBuffer( 0,__uuidof(ID3D11Texture2D),&pBackBuffer ) );
+	GFX_THROW_INFO( pDevice->CreateRenderTargetView( pBackBuffer.Get(),nullptr,&pTarget));
 }
 
 void Graphics::EndFrame()
@@ -110,7 +89,7 @@ void Graphics::EndFrame()
 void Graphics::ClearBuffer( float red,float green,float blue ) noexcept
 {
 	const float color[] = { red,green,blue,1.0f };
-	pContext->ClearRenderTargetView( pTarget,color );
+	pContext->ClearRenderTargetView( pTarget.Get(),color);
 }
 
 // Graphics Exception stuff
