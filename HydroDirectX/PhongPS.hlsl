40c4827f2ed7b6fdb92f35c1025c279370777dd9
viktor schewenius <viktorschewenius@gmail.com>
2023-01-25 19:10:56 +0100

    Added attenuiation for the Phong Pixel Shader

--- HydroDirectX/PhongPS.hlsl
+++ HydroDirectX/PhongPS.hlsl
@@ -22,15 +22,15 @@ float4 main( float3 worldPos : Position, float3 n : Normal ) : SV_Target
     const float3 vToL = lightPos - worldPos;
     const float distToL = length( vToL );
     const float3 dirToL = vToL / distToL;
-    //Diffuse Attenuation
-    const float att = 1 / (attConst + attLin * distToL + attQuad * (distToL * distToL));
+    //Attenuiation
+    const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
     //Diffuse Intensity
     const float3 diffuse = diffuseColor * diffuseIntensity * att * max( 0.0f, dot( dirToL, n ) );
     //Reflected Light Vector
     const float3 w = n * dot( vToL, n );
     const float3 r = w * 2.0f - vToL;
     //Calculate Specular Intensity Based on Angle Between Viewing Vector And Reflection Vector, Narrow With Power
-    const float3 specular = (diffuseColor * diffuseIntensity) * specularIntensity * pow( max( 0.0f, dot( normalize( -r ), normalize( worldPos ) ) ), specularPower );
+    const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow( max( 0.0f, dot( normalize( -r ), normalize( worldPos ) ) ), specularPower );
     //Final Color
     return float4( saturate( diffuse + ambient + specular) * materialColor, 1.0f );
 	
